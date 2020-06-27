bae6452ba3cab33122c115979f65d572553b95c4
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-27 16:49:50 +0900

    Initial Commit

---
+++ Swift5WKWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5WKWebView
+//
+//  Created by 野口晃 on 2020/06/27.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
